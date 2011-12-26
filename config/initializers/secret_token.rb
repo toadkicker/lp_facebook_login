0ef5d9de5d181687b98a761ca26bf3db84412aca
Todd Baur <todd@toadkicker.com>
2011-12-26 15:01:04 -0500

    Scaffold app with Devise/Omniauth/Facebook login
    For this, add your Facebook App_ID and App_Secret in
    initializers/devise.rb and test. Good luck!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoginExample::Application.config.secret_token = '4c4dc836b3f31e240c3b96dc3ca60fb4a8e73d88cded9fc0ef04db228ef0bb11e287703008f35cb6a7df50281ecd69cb533e5b8d2c689ce061ffb573d573de98'
